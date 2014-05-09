e2f0689a3c8137d322e88b387c440f67238f6cec
Tony Tamplin <tony.t@impos.com.au>
2014-05-09 10:47:42 +1000

    Add a flash message when yanking the card url

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,23 +1,42 @@
 chrome.extension.sendMessage({}, function(response) {
 
-	var readyStateCheckInterval = setInterval(function() {
-		if (document.readyState === "complete") {
+  var flashMessage = function(card, message){
+    var position = card.offset();
+    $('body').append('<div id="floatingDiv">' + message + '</div>');
+    $("#floatingDiv").css({
+      "position": "absolute",
+      "top": position.top + 5,
+      "left": position.left + (card.width() / 3),
+      "color": "white",
+      "background-color": "rgba(40, 50, 75, 0.65)",
+      "padding": "5px"
+    })
+      .fadeIn( function(){
+        $("#floatingDiv").fadeOut(function(){
+          $("#floatingDiv").remove();
+        })
+      });
+  };
 
-			clearInterval(readyStateCheckInterval);
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-	    document.onkeypress = function(e) {
+      document.onkeypress = function(e) {
 
-	      // press y on active card to 'yank' its url to the clipboard
-	      if(e.which === 121) {
+        // press y on active card to 'yank' its url to the clipboard
+        if(e.which === 121) {
 
-	        if(document.querySelector('.active-card')){
-	          var url = document.querySelector('.active-card .list-card-details > a').href;
-	          url = url.substr(0, url.lastIndexOf('/'));
+          var card = $('.active-card');
+          if(card.length === 1){
+            var url = $('.list-card-details > a', card)[0].href;
+            url = url.substr(0, url.lastIndexOf('/'));
+            flashMessage(card, 'Copied');
 
-	          chrome.extension.sendMessage({ text: url });
-	        }
-	      }
-	    };
-		}
-	}, 10);
+            chrome.extension.sendMessage({ text: url });
+          }
+        }
+      };
+    }
+  }, 10);
 });
