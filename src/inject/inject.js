27987898f8fcb41f37d1f35364ed0190a2006808
Bulkan Evcimen <bulkan@gmail.com>
2014-05-14 11:38:09 +1000

    Added ctrl+m shortcut
    * At the moment it opens the Card Menu dialog.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,5 +1,6 @@
 chrome.extension.sendMessage({}, function(response) {
 
+  // Create a flash message
   var flashMessage = function(card, message){
     var position = card.offset();
     $('body').append('<div id="floatingDiv">' + message + '</div>');
@@ -11,11 +12,11 @@ chrome.extension.sendMessage({}, function(response) {
       "background-color": "rgba(40, 50, 75, 0.65)",
       "padding": "5px"
     })
-      .fadeIn( function(){
-        $("#floatingDiv").fadeOut(function(){
-          $("#floatingDiv").remove();
-        })
-      });
+    .fadeIn(function(){
+      $("#floatingDiv").fadeOut(function(){
+        $("#floatingDiv").remove();
+      })
+    });
   };
 
   var readyStateCheckInterval = setInterval(function() {
@@ -23,20 +24,47 @@ chrome.extension.sendMessage({}, function(response) {
       clearInterval(readyStateCheckInterval);
 
       document.onkeypress = function(e) {
+        var card = $('.active-card');
 
         // press y on active card to 'yank' its url to the clipboard
         if(e.which === 121) {
 
-          var card = $('.active-card');
-          if(card.length === 1){
-            var url = $('.list-card-details > a', card)[0].href;
-            url = url.substr(0, url.lastIndexOf('/'));
-            flashMessage(card, 'Copied');
-
-            chrome.extension.sendMessage({ text: url });
+          if(card.length !== 1){
+            return;
           }
+
+          var url = $('.list-card-details > a', card)[0].href;
+          url = url.substr(0, url.lastIndexOf('/'));
+          flashMessage(card, 'Copied');
+
+          chrome.extension.sendMessage({ text: url });
+        }
+        // ctrl+m
+        else if (e.ctrlKey && e.which === 13){
+          if(card.length !== 1){
+            return;
+          }
+
+          console.log('ctrl+m');
+          card.find('span.list-card-operation').click();
+
+          setTimeout(function(){
+            var moveCardInterval = setInterval(function(){
+              var elm = $('.js-move-card').click();
+
+              if ($('span.header-title:contains("Move Card")').length > 0){
+                console.log('here')
+                clearInterval(moveCardInterval);
+              }
+
+            }, 100)
+
+
+          }, 300)
+
         }
       };
+
     }
   }, 10);
 });
