c40d48fd4ccb172a0d83cb907144d65619e6209a
Tony T <tonytamps@gmail.com>
2014-09-08 18:12:21 +1000

    refactored shortcuts to use chrome keyboard api

--- src/bg/background.js
+++ src/bg/background.js
@@ -3,7 +3,6 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
   // if the message has text then copy it to the clipboard
   if (message.hasOwnProperty('text')){
     var text = message.text;
-
     var input = document.getElementById('url');
 
     input.value = text;
@@ -15,8 +14,14 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
   sendResponse();
 });
 
-
-
 chrome.commands.onCommand.addListener(function(command) {
-  console.log('Command:', command);
+  chrome.tabs.query({
+    url: "https://trello.com/*", 
+    active: true, 
+    lastFocusedWindow: true,
+    status: "complete"
+  }, function(tabs) {
+    if (!tabs || !tabs.length) return;
+    return chrome.tabs.sendMessage(tabs[0].id, { command: command });
+  });  
 });
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,7 +1,87 @@
 chrome.extension.sendMessage({}, function(response) {
 
-  // Create a flash message
-  var flashMessage = function(card, message){
+  var elm, card, readyStateCheckInterval;
+
+  readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
+      chrome.runtime.onMessage.addListener(routeCommand);
+    }
+  }, 10);
+
+  function routeCommand(request, sender, sendResponse) {
+    card = $('.active-card');
+    switch(request.command) {
+        case 'movecard':
+          movecard();
+        break;
+        case 'yank':
+          yank();
+        break;
+        case 'movecardup':
+          movecardup();
+        break;
+        case 'notifications':
+          notifications();
+        break;
+        case 'scrolltop':
+          scrolltop();
+        break;
+        case 'scrollbottom':
+          scrollbottom();
+        break;
+    }
+  }
+
+  function movecard() {
+    if(card.length !== 1) return;
+
+    card.find('span.list-card-operation').trigger('click');
+    elm = document.querySelector('a.js-move-card');
+
+    elm.click();
+  }
+
+  function yank() {
+    if(card.length !== 1) return;
+
+    var url = $('.list-card-details > a', card)[0].href;
+    url = url.substr(0, url.lastIndexOf('/'));
+    flashMessage(card, 'Copied');
+
+    chrome.extension.sendMessage({ text: url });
+  }
+
+  function movecardup() {
+    if(card.length !== 1) return;
+
+    card.find('span.list-card-operation').trigger('click');
+    elm = document.querySelector('a.js-move-card');
+
+    elm.click();
+    $('.js-select-position').children().first().attr('selected', 'selected');
+    $('input[value="Move"]').click();
+  }
+
+  function notifications() {
+    document.querySelector('.header-notifications.js-open-header-notifications-menu').click();
+  }
+
+  function scrolltop() {
+    var cardList = $(':hover').last().parents('.list').children('.list-cards');
+    if(cardList){
+      cardList.scrollTop(0);
+    }
+  }
+
+  function scrollbottom() {
+    var cardList = $(':hover').last().parents('.list').children('.list-cards');
+    if(cardList){
+      cardList.scrollTop(cardList.height() + 500); //Just to make sure we get the entire height
+    }
+  }
+
+  function flashMessage(card, message){
     var position = card.offset();
     $('body').append('<div id="floatingDiv">' + message + '</div>');
     $("#floatingDiv").css({
@@ -17,77 +97,6 @@ chrome.extension.sendMessage({}, function(response) {
         $("#floatingDiv").remove();
       })
     });
-  };
+  }
 
-  var elm;
-
-  var readyStateCheckInterval = setInterval(function() {
-
-    if (document.readyState === "complete") {
-      clearInterval(readyStateCheckInterval);
-
-      document.onkeypress = function(e) {
-        var card = $('.active-card');
-
-        // press y on active card to 'yank' its url to the clipboard
-        if(e.which === 121) {
-
-          if(card.length !== 1){
-            return;
-          }
-
-          var url = $('.list-card-details > a', card)[0].href;
-          url = url.substr(0, url.lastIndexOf('/'));
-          flashMessage(card, 'Copied');
-
-          chrome.extension.sendMessage({ text: url });
-        }
-        // ctrl+m
-        else if (e.ctrlKey && e.which === 13){
-          if(card.length !== 1){
-            return;
-          }
-
-          card.find('span.list-card-operation').trigger('click');
-          elm = document.querySelector('a.js-move-card');
-
-          // not a jquery event ? or some sort of weird event bubble bug
-          elm.click();
-        }
-
-        // ctrl+u
-        else if (e.ctrlKey && e.which === 21) {
-          if(card.length !== 1){
-            return;
-          }
-
-          card.find('span.list-card-operation').trigger('click');
-          elm = document.querySelector('a.js-move-card');
-          // not a jquery event ? or some sort of weird event bubble bug
-          elm.click();
-          $('.js-select-position').children().first().attr('selected', 'selected');
-          $('input[value="Move"]').click();
-        }
-        //ctrl+n for notifications popup
-        else if (e.ctrlKey && e.which === 14){
-          document.querySelector('.header-notifications.js-open-header-notifications-menu').click();
-        }
-        //ctrl+up ScrollTop
-        else if (e.ctrlKey && e.which === 63232){
-          var cardList = $(':hover').last().parents('.list').children('.list-cards');
-          if(cardList){
-            cardList.scrollTop(0);
-          }
-        }
-        //ctrl+down ScrollBottom
-        else if (e.ctrlKey && e.which === 63233){
-          var cardList = $(':hover').last().parents('.list').children('.list-cards');
-          if(cardList){
-            cardList.scrollTop(cardList.height() + 500); //Just to make sure we get the entire height
-          }
-        }
-      };
-
-    }
-  }, 10);
 });
